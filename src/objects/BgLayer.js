35686682dc83ddffc46c1c28e4c1812e38628c47
Lindsay Gaines <lindsay.gaines@gmail.com>
2016-04-18 17:58:25 +1000

    Fix parallax scroll speed and add vertical offsetting

--- src/objects/BgLayer.js
+++ src/objects/BgLayer.js
@@ -13,11 +13,14 @@ export default class BgLayer extends Phaser.TileSprite {
     super(game, x, y, width, height, key);
 
     this.moveMultiplier = moveMultiplier;
-
+    this.fixedOffset = new Phaser.Point(x, y);
+    this.fixedToCamera = true;
+    this.cameraOffset = new Phaser.Point(this.fixedOffset.x, this.fixedOffset.y);
   }
 
   update () {
-    this.tilePosition.x = this.game.camera.x * this.moveMultiplier;
+    this.tilePosition.x = -(this.game.camera.x * this.moveMultiplier);
+    this.cameraOffset.y = -(this.game.camera.y * this.moveMultiplier) + this.fixedOffset.y ;
   }
 
 }
